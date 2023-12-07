6798d58040e46651140905d2b3301110a59103da
Adedokun Timileyin <adedokuntimileyin123@gmail.com>
2023-12-07 11:44:24 +0000

    input zoom fix..2

--- app/components/Home-Components/SearchInput.tsx
+++ app/components/Home-Components/SearchInput.tsx
@@ -1,5 +1,5 @@
 import { Cartprops } from '@/app/interface';
-import React from 'react'
+import React, { useEffect } from 'react'
 
 interface Props {
     search: string;
@@ -9,6 +9,27 @@ interface Props {
 }
 
 function SearchInput({search, setSearch, setIsSearch} : Props) {
+
+
+
+  useEffect(() => {
+    document.addEventListener('touchstart', function(e) {
+      if (document.activeElement?.tagName !== 'INPUT') {
+        document.body.style.overflow = 'hidden';
+      }
+    });
+    
+    document.addEventListener('touchend', function(e) {
+      document.body.style.overflow = 'visible';
+    });
+    
+
+
+
+  }, [])
+  
+
+
   return (
     <input  type='text' placeholder='Chicken' onFocus={()=> setIsSearch(true)} value={search} onChange={(e)=>setSearch(e.target.value)}  className='p-2 bg-slate-200 rounded-md text-sm w-full focus-within:outline-none'/>
   )
